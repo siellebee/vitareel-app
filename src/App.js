6406a9600e174bfa7642e4fe60501cb9b0425f16
siellebee <shesielle@gmail.com>
2026-05-31 21:01:48 -0400

    fix eslint warning

--- src/App.js
+++
@@ -1,4 +0,0 @@
-import { useState } from "react";
-export default function App() {
-  return <div style={{padding:40,fontFamily:"sans-serif",background:"#faf8f4",minHeight:"100vh"}}><h1>VitaReel Loading...</h1></div>;
-}
