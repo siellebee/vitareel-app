8eed1d9a7b9a851ac0cb5448e5005cdcba0c21be
siellebee <shesielle@gmail.com>
2026-05-31 20:46:31 -0400

    initial vitareel app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useState } from "react";
+export default function App() {
+  return <div style={{padding:40,fontFamily:"sans-serif",background:"#faf8f4",minHeight:"100vh"}}><h1>VitaReel Loading...</h1></div>;
 }
-
-export default App;
